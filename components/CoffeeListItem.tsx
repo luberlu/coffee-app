bea849c85e622a08f621f012e43d7852c71d3590
luberlu <hello@luberlu.me>
2024-12-12 17:46:24 +0100

    add add to cart button ui

--- components/CoffeeListItem.tsx
+++ components/CoffeeListItem.tsx
@@ -1,32 +1,66 @@
-import { Link } from 'expo-router';
+import { Link } from "expo-router";
 import { Text, View } from "./Themed";
-import { Pressable, StyleSheet, Image } from 'react-native';
-import { useQuery } from '@tanstack/react-query';
-import { constants } from '@/constants/Colors';
+import { Pressable, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { useQuery } from "@tanstack/react-query";
+import Colors, { constants } from "@/constants/Colors";
+import Plus from '@/assets/icons/plus.svg';
 
-export function CoffeeListItem({ id, name, type, price }: { id: string, name: string, type: string, price: number }) {
-
+export function CoffeeListItem({
+  id,
+  name,
+  type,
+  price,
+}: {
+  id: string;
+  name: string;
+  type: string;
+  price: number;
+}) {
   const { data: imageUrl } = useQuery({
-    queryKey: ['coffee-image', id],
-    queryFn: () => fetch(`https://coffee.alexflipnote.dev/random?id=${id}`).then(res => res.url),
+    queryKey: ["coffee-image", id],
+    queryFn: () =>
+      fetch(`https://coffee.alexflipnote.dev/random?id=${id}`).then(
+        (res) => res.url
+      ),
   });
 
   return (
     <Link href={`/(app)/coffee/${id}`} asChild>
       <Pressable style={styles.container}>
         <View style={styles.imageContainer}>
-          <Image 
-            source={{ uri: imageUrl }}
-            style={styles.image}
-          />
+          <Image source={{ uri: imageUrl }} style={styles.image} />
         </View>
         <Text style={styles.title}>{name}</Text>
         <Text style={styles.type}>{type}</Text>
-        <Text style={styles.price}>{price}$</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            backgroundColor: "transparent",
+            alignItems: "center",
+          }}
+        >
+          <Text style={styles.price}>{price}$</Text>
+          <AddToCart />
+        </View>
       </Pressable>
     </Link>
   );
 }
+
+const AddToCart = () => {
+  return (
+    <TouchableOpacity 
+      style={styles.addToCartButton}
+      onPress={() => {
+        // Ajouter la logique pour ajouter au panier ici
+        console.log("Ajouté au panier");
+      }}
+    >
+      <Plus />
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -52,18 +86,24 @@ const styles = StyleSheet.create({
   },
   price: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: 5,
   },
   title: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 5,
   },
   type: {
     fontSize: 14,
-    color: '#666',
+    color: "#666",
     marginBottom: 5,
   },
+  addToCartButton: {
+    backgroundColor: Colors.light.primary,
+    padding: 8,
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
-
